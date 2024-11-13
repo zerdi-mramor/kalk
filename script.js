888561e981caa75b5f0f965f558e0fc854151219
zerdi-mramor <Sarkicartem5@gmail.com>
2024-11-13 19:55:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+
+function addition () {
+    let numberOne = document.getElementById('myForm1').valueAsNumber;
+    let numberTwo = document.getElementById('myForm2').valueAsNumber;
+    let resultWindow = document.getElementById('result');
+    resultWindow.innerHTML = numberOne+ numberTwo;
+  console.log(result)
+}
+
+function substraction () {
+    let numberOne = document.getElementById('myForm1').valueAsNumber;
+    let numberTwo = document.getElementById('myForm2').valueAsNumber;
+    let resultWindow = document.getElementById('result');
+    resultWindow.innerHTML = numberOne- numberTwo;
+  console.log(result)
+}
+
+function multiplication () {
+    let numberOne = document.getElementById('myForm1').valueAsNumber;
+    let numberTwo = document.getElementById('myForm2').valueAsNumber;
+    let resultWindow = document.getElementById('result');
+    resultWindow.innerHTML = numberOne * numberTwo;
+  console.log(result)
+}
+
+function division () {
+    let numberOne = document.getElementById('myForm1').valueAsNumber;
+    let numberTwo = document.getElementById('myForm2').valueAsNumber;
+    let resultWindow = document.getElementById('result');
+    resultWindow.innerHTML = numberOne / numberTwo;
+  console.log(result)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
